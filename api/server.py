3f61187ee7bc7c339753b55648ffaeb7003f32de
Seema Ullal <seemaullal@gmail.com>
2021-12-11 12:27:36 -0800

    add production routing

--- api/server.py
+++ api/server.py
@@ -2,11 +2,20 @@ from flask import Flask, jsonify, send_from_directory
 from time import sleep
 from data import DATA
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../build", static_url_path="/")
 app.secret_key = "secret"
 
 
-# TBD: Add route for production
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def index(path):
+    return app.send_static_file("index.html")
+
+
+@app.errorhandler(404)
+def not_found(_error):
+    return app.send_static_file("index.html")
+
 
 @app.route("/api/map_data")
 def get_map_data():
@@ -16,4 +25,4 @@ def get_map_data():
 
 
 if __name__ == "__main__":
-    app.run("0.0.0.0", debug=True, port=5001) # will run on port 5000 by default
+    app.run("0.0.0.0", debug=True, port=5001)  # will run on port 5000 by default
